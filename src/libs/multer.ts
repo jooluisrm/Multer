556951631b70b0f35cdf96fa234a33a473410392
João Luís Rodrigues de Moura <joaoluis4633@gmail.com>
2025-04-29 10:30:23 -0300

    Configurando DiskStorage

--- src/libs/multer.ts
+++ src/libs/multer.ts
@@ -1,5 +1,17 @@
 import multer from "multer";
 
+const diskStorage = multer.diskStorage({
+    filename: (req, file, cb) => {
+        const prefix = 'img-' + Math.floor(Math.random() * 9999);
+        console.log(file);
+        cb(null, prefix+'.jpg');
+    },
+    destination: (req, file, cb) => {
+        cb(null, './public/uploads/tmp');
+    }
+
+})
+
 export const upload = multer({
-    dest: './public/uploads',
+    storage: diskStorage
 });

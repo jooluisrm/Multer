2d54768d26a64f0edf3cb9ab055aa4745b060460
João Luís Rodrigues de Moura <joaoluis4633@gmail.com>
2025-04-30 09:38:37 -0300

    Colocando uma marca d'agua na imagem

--- src/controllers/upload.ts
+++ src/controllers/upload.ts
@@ -7,7 +7,15 @@ export const upload: RequestHandler = async (req, res) => {
 
     if (req.file) {
         const newName = v4() + '.jpg';
-        const image = await sharp(req.file.path).resize(1280, 720, { fit: "cover" }).toFormat('jpg').toFile('./public/images/' + newName);
+        const image = await sharp(req.file.path)
+        .resize(1280, 720, { fit: "cover" })
+        .composite([
+            {
+                input: './src/assets/logoMBLIVRE.png', gravity: 'centre'
+            }
+        ])
+        .toFormat('jpg')
+        .toFile('./public/images/' + newName);
 
         await fs.unlink(req.file.path); // Remove o arquivo temporário após o processamento
 
